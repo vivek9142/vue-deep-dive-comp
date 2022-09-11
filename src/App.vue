e44c38fe611ab3c6dc629b31086d903d64255a4b
Vivek <vivek9142@gmail.com>
2022-09-11 15:42:57 +0530

    added local and global components

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,33 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+/*
+Now components wants an object, and then we need a key-value pair,
+where the key is our custom HTML element. So TheHeader for example, the tag we wanna use
+for this element, so to say, and then the value is the imported component config object, 
+we're pointing at. And If I do that, and Save this, this still all works.
+
+But now TheHeader is registered locally. And you can actually also write this differently.
+This is perfectly fine, but you can also use TheHeader like "the-Header" as a key here,
+as a property name and you will still be able to use it like this in your template.
+Vue automatically translates this so that you can use the-header in your template
+or in case you prefer it, TheHeader like this. And then you can also write it
+as a self-closing element if you want to. This self-closing tag is not supported
+if you write it with a dash in between, then this self-closing tag will not work
+if you write it in this PascallCase it works
+*/
 export default {
+  components:{
+    // TheHeader: TheHeader or TheHeader
+    'the-header':TheHeader,
+    // it can work here in comp as in html as badge-list(open n close tag) or BadgeList(self close)
+    BadgeList, 
+    UserInfo
+  },
   data() {
     return {
       activeUser: {
--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
 
-import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
-import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import App from "./App.vue";
+import BaseBadge from "./components/BaseBadge.vue";
 
 const app = createApp(App);
 
-app.component('the-header', TheHeader);
-app.component('base-badge', BaseBadge);
-app.component('badge-list', BadgeList);
-app.component('user-info', UserInfo);
+/*
+when we register components like this with the component method on our App,
+then we are registering those components as so called Global Components.
+That means they're available globally in this entire Vue App.
+So we can use these components as custom HTML elements anywhere in this App.
 
-app.mount('#app');
+if you're registering them all globally, that in the end means Vue needs to load them all
+when the application is loaded initially. So the browser needs to download the code
+for all those components initially.
+
+we could remove this component registration in main.js, and remove this import here.
+And instead go to our App.Vue file, which is where we plan on using this.
+And then here in the script section, we can import TheHeader from components TheHeader.Vue.
+And now we can also register this component locally in this App component or in this App 
+object here in this App options object.
+*/
+// app.component("the-header", TheHeader);
+app.component("base-badge", BaseBadge);
+// app.component("badge-list", BadgeList);
+// app.component("user-info", UserInfo);
+
+app.mount("#app");
