9749c5de3e0adfd3983fc3ba49c4afe74a1f03a8
Vivek <vivek9142@gmail.com>
2022-09-12 03:01:16 +0530

    added teleport

--- src/App.vue
+++ src/App.vue
@@ -69,21 +69,33 @@ body {
 }
 </style>
 
+visually, everything is all right. But if I inspect this here,
+we can see a problem in the HTML mark-up. Here's our dialogue right?
+Its below the button input and h2 tag. And its below there, because that's where we added it
+in the template of Manage Goals.Now visually as I said everything works but semantically,
+and from HTML perspective, having this, element the dialogue,
+which actually is treated as overlay to the entire page. Having that nested here,
+in our HTML code, is not ideal.It works but for accessibility this could be strange.
+You could also run into some styling bugs here
 
-Let me stick to this active goals component And let's say here, we have a button.
-Where I have a text of set goal,and when this button is clicked, I wanna save my goal.
+And this is something which is easy to achieve with vue, and a feature called Teleport.
+Teleport, is a built in vue component just like component and keep alive.
+You can wrap your to-be teleported thing in this case the error alert,
+with the built-in Teleport element. Which again is a built-in component vue provides to you.
 
-I get my alert here. This works, but in most applications,
-you probably don't wanna show an alert like this. This built in alert is not really nice to 
-look at, it probably doesn't fit the styling of the rest of your application,
-and therefore, this is maybe not what you wanna do. Instead, maybe you want to build your 
-own alert, your own model. So your own overlay dialogue, which you wanna show.
-And that's exactly what I'll do here.
+Now how does Teleport work or what do you with it. Teleport wants one prop, one attribute.
+The "to" prop. And here you provide a CSS selector, selecting an HTML element onto your 
+entire page, where this content should actually be added to in the HTML mark-up.
 
-I'll build my ErrorAlert.vue component here.
+So logically, it still belongs to this component you can still interact with methods 
+in that component and so on. But it will be rendered somewhere else in the dom's structure.
 
+Go to ActiveGoals.vue
 
-So we don't need to pass data around with custom events, which the error alert emits here.
-Instead, we can handle everything here in the component where the error is generated and 
-yet hand off the styling and the markup off that dialogue to a totally different component.
-Which is a really nice way of splitting responsibility.
+And here for example we could select the body element by just referring to the body tag.
+You could also refer to an element by an ID selector like this. You just pass in a regular 
+CSS selector here. And here I'll go for the body tag.
+
+But if we inspect this now, we see this dialogue is now at the end of the body section.
+And its no longer deeply nested in this div or any other elements. And semantically this 
+can be better.
--- src/components/ActiveGoals.vue
+++ src/components/ActiveGoals.vue
@@ -4,11 +4,13 @@
     <div>
         <input type="text" ref="goal">
         <button @click="setGoal">Set Goal</button>
-        <error-alert v-if="inputIsInvalid">
+        <teleport to="body">
+            <error-alert v-if="inputIsInvalid">
             <h2>Input is Invalid</h2>
             <p>Please enter at least a few characters.</p>
             <button @click="confirmError">Okay</button>
         </error-alert>
+        </teleport>
     </div>
     </div>
 </template>
