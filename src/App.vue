6faa65cf215a868b65b3104fe27f4fa7ac2f8725
Vivek <vivek9142@gmail.com>
2022-09-12 02:18:33 +0530

    added scoped slots

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{slotProps.item}}</h2>
+        <p>{{slotProps.anotherProp}}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,12 +20,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components:{
     'the-header':TheHeader,
     BadgeList, 
-    UserInfo
+    UserInfo,
+    CourseGoals
   },
   data() {
     return {
